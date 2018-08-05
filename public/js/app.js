9d335353fab15b4103b5ed85fcd8075baa6a1d89
meganmuirhead <megan@s56.net>
2018-08-04 23:22:25 -0600

    1,personal space

--- public/js/app.js
+++ public/js/app.js
@@ -203,8 +203,7 @@ class Timer extends React.Component {
 
     makingFunc = (e) => {
         console.log('making func clicked', e);
-        console.log('still not working', e);
-        console.log('never gunna work', e);
+     
 
 
         this.props.elapsed = 1;
